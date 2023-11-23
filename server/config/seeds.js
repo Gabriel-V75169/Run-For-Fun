dd74d6f83913bfca2c55386410a222700e35474d
Justin Bobrick <j.bobrick@outlook.com>
2023-11-22 19:33:38 -0800

    debugging server.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -8,11 +8,8 @@ db.once('open', async () => {
   await cleanDB('User', 'users');
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Races' },
+    { name: 'Distance' }
   ]);
 
   console.log('categories seeded');
@@ -22,18 +19,18 @@ db.once('open', async () => {
       name: '12 Ks of Christmas',
       description:
         'A fun, festive romp through the 12 Ks of Christmas. A great gift for the runner in your life!',
-      image: 'cookie-tin.jpg',
+      image: '/images/12KsSNOW_2048x2048.jpg',
       category: categories[0]._id,
-      price: 2.99,
+      price: 25.99,
       quantity: 500
     },
     {
       name: 'The Infinity Gauntlet',
       description:
         'A race medal worthy of the Mad Titan himself. This medal is sure to be the crown jewel of your collection.',
-      image: 'canned-coffee.jpg',
+      image: '/images/InfinityGauntlet(4).jpg',
       category: categories[0]._id,
-      price: 1.99,
+      price: 25.99,
       quantity: 500
     },
     {
@@ -41,98 +38,36 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'A tough race consisting of 26 grueling miles. Put yourself to the ultimate test!',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+      image: '/images/MarathonChallenge.png',
+      price: 25.99,
+      quantity: 500
     },
     {
       name: 'Zombie Run',
       category: categories[1]._id,
       description:
         'The dead have risen and are hungry for brains. Can you outrun them?',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+      image: '/images/MZMBRN_NR.jpg',
+      price: 25.99,
+      quantity: 500
     },
     {
       name: 'The Shire Challenge',
       category: categories[1]._id,
       description:
         'A peaceful run through the Shire. Be sure to stay on the path!',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
-      category: categories[2]._id,
-      description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
-    },
-    {
-      name: 'Tablet',
-      category: categories[2]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
-    },
-    {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
-    },
-    {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+      image: '/images/SHR-Front-Medal-min.png',
+      price: 25.99,
+      quantity: 500
     }
   ]);
 
   console.log('products seeded');
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
+    firstName: 'Justin',
+    lastName: 'Bobrick',
+    email: 'justin@testmail.com',
     password: 'password12345',
     orders: [
       {
@@ -142,9 +77,9 @@ db.once('open', async () => {
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'Andrew',
+    lastName: 'Stave',
+    email: 'andrew@testmail.com',
     password: 'password12345'
   });
 
